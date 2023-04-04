834bc28a74686d72992a7a4239543ecb1e4f3f86
mortazah101 <m.alizada@sintlucasedu.nl>
2023-04-04 10:41:59 +0200

    1

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const offsetCharacter = 16;
 const characterAudio = document.getElementById("characterAudio");
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
-
+const mainCharacterSpeech =  document.getElementById("mainCharacterSpeech");
+const counterspeech = document.getElementById("counterspeech");
+const sec = 1000;
+hidehero();
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
@@ -13,24 +16,71 @@ gameWindow.onclick = function (e) {
     mainCharacter.style.left = x - offsetCharacter + "px";
     mainCharacter.style.top = y - offsetCharacter + "px";
 
-    console.log(x);
+    showSpeech("what is the password?")
+    hideSpeech();
 
-    console.log(e.target.id);
+  
     switch (e.target.id) {
-        case "doorOne":
-            characterAudio.play();
-            console.log("this is door one");
+        case "door1":
+        showhero();
+        showSpeech("What is the password?");
             // something insert here
             break;
-
-        case "doorTwo":
+        case "hero":
+        
+        showSpeech("What is the password?");
+            // something insert here
+            break; 
+        case "door2":
             console.log("this is door two");
+            
             // something insert here
             break;
 
         default:
-            console.log("unavailable clicked item");
+            hidehero();
+            hideSpeech();
+        
             // do something when it doesn't have a case
             break;
     }
+}
+
+function showSpeech (  dialog){
+
+    // trigger speech and audio // 
+    mainCharacterSpeech.style.opacity = 1;
+    mainCharacterSpeech.innerText = dialog;
+
+    // doesnt display after 5 sec //
+    setTimeout(hideSpeech, sec * 4 );
+    ///setTimeout(hidehero,  sec * 5);
+    
+    
+}
+function hideSpeech(){
+    mainCharacterSpeech.style.opacity = 0;
+    mainCharacterSpeech.innerText = "  ";
+    
+
+}
+function hidehero(){
+    hero.style.opacity = 0;
+    mainCharacterSpeech.innerText = "  ";
+
+}
+function showhero(){
+    hero.style.opacity = 1;
+    mainCharacterSpeech.innerText = "  ";
+
+}
+
+function playAudio() {
+    characterAudio();
+
+
+}
+//function showSpeech(counterspeech, characterAudio, )
+{
+
 }
